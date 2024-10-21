b2259524f23b6c3a850194115b487610e0ef732a
Jonathan Lindquist <26193336+jonathanLindquist@users.noreply.github.com>
2024-10-21 12:07:00 -0500

    build

--- section_2/react_crash_course/src/components/Post.jsx
+++ section_2/react_crash_course/src/components/Post.jsx
@@ -1,8 +1,12 @@
-function Post(props) {
-  return <div>
-    <p>{props.author}</p>
-    <p>{props.body || "Default body, everything is awesome"}</p>
-  </div>;
-}
+const Post = (props) => {
+  console.log(`arrow this value: ${this}`);
+
+  return (
+    <div>
+      <p>{props.author}</p>
+      <p>{props.body || 'Default body, everything is awesome'}</p>
+    </div>
+  );
+};
 
 export default Post;
